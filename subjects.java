0706832f3b69b9f9c651347e5d96d8b2c1cd11d5
Rehaan Arai <42904587+amanarai17@users.noreply.github.com>
2019-11-04 22:58:33 +0530

    Add files via upload

--- subjects.java
+++ subjects.java
@@ -6,10 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+
 
 public class subjects extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel contentPane;  
 
 	/**
 	 * Launch the application.
@@ -26,6 +37,7 @@ public class subjects extends JFrame {
 			}
 		});
 	}
+	
 
 	/**
 	 * Create the frame.
@@ -35,8 +47,49 @@ public class subjects extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		JButton btnAddNewSubject = new JButton("Add new book");
+		btnAddNewSubject.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				newbook nb = new newbook();
+				nb.setVisible(true);
+				
+				
+				
+					// TODO: handle exception
+				
+				
+			}
+		});
+		btnAddNewSubject.setBounds(82, 192, 143, 23);
+		contentPane.add(btnAddNewSubject);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				allfields all = new allfields();
+				all.setVisible(true);
+				
+			}
+		});
+		btnBack.setBounds(253, 192, 89, 23);
+		contentPane.add(btnBack);
+		
+		JLabel lblChooseASubject = new JLabel("Choose a subject");
+		lblChooseASubject.setBounds(82, 34, 134, 39);
+		contentPane.add(lblChooseASubject);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.addItem("Data Structures");
+		comboBox.addItem("FAFL");
+		comboBox.addItem("Computer Organization");
+		comboBox.addItem("Computer Networks");
+		comboBox.setSelectedItem(null);
+		
+		comboBox.setBounds(82, 100, 134, 22);
+		contentPane.add(comboBox);
 	}
-
 }
